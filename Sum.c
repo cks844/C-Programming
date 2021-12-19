1ae3b8d32a8dad72889459f80b701377cf5d7e50
CYRIL  K  SONY <84973835+CyrilKSony@users.noreply.github.com>
2021-12-18 23:07:02 -0800

    Sum of two integers using function

--- Sum.c
+++ Sum.c
@@ -2,12 +2,12 @@
 int sum(int,int );
 int main()
 {
-    int num1,num2,c;
+    int num1,num2,result;
     do
     {
     printf("Enter two numbers num1,num2:");
     scanf("%d %d",&num1,&num2);
-    c=sum(num1,num2);
+    result=sum(num1,num2);
     printf("The sum of two numbers is %d",c);
     printf("\n Do you want to continue(If YES,please type Y or y):");
     scanf(" %c",&c);
