32c82ee86d987fbdda9c971cb476fdbe7696010d
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2022-01-21 09:39:43 -0800

    Sum of elements in an array

---
+++ Array_sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],n,sum=0,i;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    printf("\nEnter the elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    printf("\n Sum of the numbers is: %d", sum);
+    return 0;
+}
