f5b61aef8706a854ac55d400dc0d454433cad9c1
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2022-01-21 09:43:59 -0800

    Calculate sum of elements in an array easily

---
+++ Array_sum_easy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],n,sum=0,i;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    printf("\nEnter the elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    sum=n*(n+1)/2;
+    printf("\n Sum of the elements is: %d", sum);
+    return 0;
+}
