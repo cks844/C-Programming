0228fc1ffa9152c7320728015c9f8e1884f86b93
CYRIL  K  SONY <84973835+cks844@users.noreply.github.com>
2022-02-03 02:14:06 -0800

    Create Number_pattern2.c

---
+++ Number_pattern2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,row;
+    printf("Enter the number of rows:");
+    scanf("%d",&row);
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=i;j++){
+        printf("%d",j);}
+        printf("\n");
+    }
+    return 0;
+}
